1f755b6dcdccab89223dd8d86c69e0bb31373ded
Vlad Ermakov <erimokvl@gmail.com>
2021-08-05 22:50:44 +0300

    UY-5: TaxonomyArgs class has been updated

--- UYCore/Taxonomies/TaxonomyArgs.php
+++ UYCore/Taxonomies/TaxonomyArgs.php
@@ -4,7 +4,6 @@ namespace UYCore\Taxonomies;
 
 use UYCore\PostTypes\Interfaces\ArrayInterface;
 
-// TODO add all args
 final class TaxonomyArgs implements ArrayInterface
 {
     const CAPABILITIES = [
@@ -20,15 +19,25 @@ final class TaxonomyArgs implements ArrayInterface
      */
     private array $labels = [];
     private bool $public = true;
+    private bool $show_ui = true;
+    private bool $show_in_menu = true;
+    private bool $show_in_nav_menus = true;
+    private bool $show_tagcloud = true;
+    private ?bool $show_in_rest = null;
+    private ?string $rest_base = null;
+    private string $rest_controller_class = \WP_REST_Terms_Controller::class;
     private bool $hierarchical = true;
     /**
      * @var bool|array
      */
     private bool $rewrite = true;
+    private ?bool $publicly_queryable = null;
     private array $capabilities = self::CAPABILITIES;
+    private ?string $meta_box_cb = null;
     private bool $show_admin_column = false;
-    private ?bool $show_in_rest = null;
-    private ?string $rest_base = null;
+    private ?bool $show_in_quick_edit = null;
+    private ?bool $sort = null;
+    private array $default_term = [];
 
     public function getArray(): array
     {
@@ -36,6 +45,16 @@ final class TaxonomyArgs implements ArrayInterface
             'description' => $this->description,
             'labels' => $this->labels,
             'public' => $this->public,
+            'show_ui' => $this->show_ui,
+            'show_in_menu' => $this->show_in_menu,
+            'show_in_nav_menus' => $this->show_in_nav_menus,
+            'show_tagcloud' => $this->show_tagcloud,
+            'rest_controller_class' => $this->rest_controller_class,
+            'publicly_queryable' => $this->publicly_queryable,
+            'meta_box_cb' => $this->meta_box_cb,
+            'show_in_quick_edit' => $this->show_in_quick_edit,
+            'sort' => $this->sort,
+            'default_term' => $this->default_term,
             'hierarchical' => $this->hierarchical,
             'rewrite' => $this->rewrite,
             'capabilities' => $this->capabilities,
@@ -114,6 +133,76 @@ final class TaxonomyArgs implements ArrayInterface
         return $this;
     }
 
+    public function setDefaultTerm(array $default_term = []): self
+    {
+        $this->default_term = $default_term;
+
+        return $this;
+    }
+
+    public function setMetaBoxCb(?string $meta_box_cb = null): self
+    {
+        $this->meta_box_cb = $meta_box_cb;
+
+        return $this;
+    }
+
+    public function setPubliclyQueryable(?bool $publicly_queryable = null): self
+    {
+        $this->publicly_queryable = $publicly_queryable;
+
+        return $this;
+    }
+
+    public function setRestControllerClass(string $rest_controller_class = \WP_REST_Terms_Controller::class): self
+    {
+        $this->rest_controller_class = $rest_controller_class;
+
+        return $this;
+    }
+
+    public function setShowInMenu(bool $show_in_menu = true): self
+    {
+        $this->show_in_menu = $show_in_menu;
+
+        return $this;
+    }
+
+    public function setShowInNavMenus(bool $show_in_nav_menus = true): self
+    {
+        $this->show_in_nav_menus = $show_in_nav_menus;
+
+        return $this;
+    }
+
+    public function setShowInQuickEdit(?bool $show_in_quick_edit = null): self
+    {
+        $this->show_in_quick_edit = $show_in_quick_edit;
+
+        return $this;
+    }
+
+    public function setShowTagcloud(bool $show_tagcloud = true): self
+    {
+        $this->show_tagcloud = $show_tagcloud;
+
+        return $this;
+    }
+
+    public function setShowUi(bool $show_ui = true): self
+    {
+        $this->show_ui = $show_ui;
+
+        return $this;
+    }
+
+    public function setSort(?bool $sort = null): self
+    {
+        $this->sort = $sort;
+
+        return $this;
+    }
+
     public function setDefaultValues(): self
     {
         $this->setCapabilities()
@@ -121,6 +210,16 @@ final class TaxonomyArgs implements ArrayInterface
             ->setHierarchical()
             ->setLabels()
             ->setPublic()
+            ->setDefaultTerm()
+            ->setMetaBoxCb()
+            ->setPubliclyQueryable()
+            ->setRestControllerClass()
+            ->setShowInMenu()
+            ->setShowInNavMenus()
+            ->setShowInQuickEdit()
+            ->setShowTagcloud()
+            ->setShowUi()
+            ->setSort()
             ->setRestBase()
             ->setRewrite()
             ->setShowAdminColumn()
